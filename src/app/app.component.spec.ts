832ab40075afe7292d3513ffe6834b9bbabe776c
Michael-IRL <iszekeell@gmial.com>
2022-02-01 22:13:11 +0000

    Add hero component and tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        FormsModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        HeroesComponent
       ],
     }).compileComponents();
   });
