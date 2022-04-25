fd63191e8c3ed302f7d97a0d5a7ea23fb2c0ec49
Michael-IRL <iszekeell@gmial.com>
2022-04-25 09:56:08 +0100

    add hero form

--- src/app/mock-heros.ts
+++ src/app/mock-heros.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Dr Nice', power: 'mind' },
+  { id: 12, name: 'Narco', power: 'dark' },
+  { id: 13, name: 'Bombasto', power: 'fire' },
+  { id: 14, name: 'Celeritas', power: 'light' },
+  { id: 15, name: 'Magneta', power: 'magnetism' },
+  { id: 16, name: 'RubberMan', power: 'flex' },
+  { id: 17, name: 'Dynama', power: 'speed' },
+  { id: 18, name: 'Dr IQ', power: 'smart' },
+  { id: 19, name: 'Magma', power: 'lava' },
+  { id: 20, name: 'Tornado', power: 'wind' },
 ];
